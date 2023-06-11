6051c59375ccd9dbdfbda298bcb45acaddd6ae6c
Artemio <Artemio@DESKTOP-92BTGL5>
2023-06-10 22:49:07 -0300

    Added Repository
    Added interface and implementation class for CRUD methods in EF.
    Added Scoped dependency injection in Program.cs file.

---
+++ Data/ICommandRepo.cs
@@ -0,0 +1,12 @@
+﻿using SixMinApi.Models;
+
+namespace SixMinApi;
+
+public interface ICommandRepo
+{
+    Task SaveChanges();
+    Task<Command?> GetCommandById(int id);
+    Task<IEnumerable<Command>> GetAllCommands();
+    Task CreateCommand(Command cmd);
+    void DeleteCommand(Command cmd);
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using SixMinApi;
 using SixMinApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,7 @@ var sqlConnBuilder = new SqlConnectionStringBuilder
 };
 
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConnBuilder.ConnectionString));
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 
 var app = builder.Build();
 
